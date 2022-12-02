6ca4991f887d261deba8cbd56e402b0b644736e3
Chaitalimane <chaitali.mane@lntinfotech.com>
2022-12-02 23:56:42 +0530

    my last UI changes

--- src/app/policy-details/policy-list.component.ts
+++ src/app/policy-details/policy-list.component.ts
@@ -36,19 +36,9 @@ export class PolicyListComponent implements OnInit {
         if(usertoken!==null){
         headers = headers.set('token', usertoken );
         this.sub = this.productService.getPolicy(headers).subscribe((dataResponse) => {
-          this.dataSource = dataResponse;
+          this.dataSource = dataResponse.policyData;
           });
-          window.location.reload();
       }
         }
 }
 
-  // const dataSource: Element[] = [
-  //   {position: 1, username: 'John Doe', policyID: '12345D12', policyCoverage:250000, policyPremium: 2000, paymentStatus: 'Completed'},
-  //   {position: 2, username: 'Mike Hussey', policyID: '4564E45', policyCoverage:125000, policyPremium: 450, paymentStatus: 'Declined'},
-  //   {position: 3, username: 'Ricky Hans', policyID: '4564F458', policyCoverage:300000, policyPremium: 1000, paymentStatus: 'Completed'},
-  //   {position: 4, username: 'Martin Kos', policyID: '4564G458', policyCoverage:4500000, policyPremium: 670, paymentStatus: 'In-Progress'},
-  //   {position: 5, username: 'Tom Paisa', policyID: '4564H458', policyCoverage:1200000, policyPremium: 430, paymentStatus: 'Completed'},
-  //   {position: 6, username: 'George Donald', policyID: '4564H458', policyCoverage:2300000, policyPremium: 1500, paymentStatus: 'Completed'},
-  //   {position: 7, username: 'Chaitali Mane', policyID: '4564H458', policyCoverage:200000, policyPremium: 2000, paymentStatus: 'In-Progress'}
-  // ];
