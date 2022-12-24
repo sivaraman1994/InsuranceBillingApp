7639937dd07e2fcced611a6dc0a3a7951f37f46a
Chaitalimane2027 <114143495+Chaitalimane2027@users.noreply.github.com>
2022-12-24 15:51:51 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,14 +6,28 @@ import { Component } from '@angular/core'
     styles: [`
       div.card-header {
         font-size: large;
-       }
-       
+       }      
        div.card {
-         width: 90%;
-         height: 450px;
-         align: center;
-         margin: 30px auto 30px auto;
-       }`]
+        width: 90%;
+        height: 750px;
+        align: center;
+        margin: 30px auto 30px auto;
+       }
+       .bg-gray-sec{
+        background-color: white;
+       }
+      .text-center{
+        text-align: center !important;
+      }
+      .px-4 {
+    padding-left: 1.5rem !important;
+}
+.plans-available .card {
+    margin: 0 13px 0 12px;
+}
+
+        
+       `]
 })
 export class HomeDetailsComponent {
     public pageTitle = 'Welcome to Trip Insurance';
